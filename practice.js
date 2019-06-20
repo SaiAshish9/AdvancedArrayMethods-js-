9e8cd12bad61d2e2214314a18a527bfc68c7d9e3
SaiAshish9 <43849911+SaiAshish9@users.noreply.github.com>
2019-06-20 20:10:24 +0530

    Add files via upload

--- practice.js
+++ practice.js
@@ -275,3 +275,132 @@ function reverseArray(arr){
   }
   return arr;
 }
+
+
+//ESE5
+
+
+
+
+// Write a method called multiplyFavoriteNumber that takes in a number and returns the product of the number and the Person's favorite number
+
+// function Person(firstName, lastName, favoriteColor, favoriteNumber){
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.favoriteColor = favoriteColor;
+//   this.favoriteNumber = favoriteNumber;
+//   this.multiplyFavoriteNumber = function(num){
+//     return num * this.favoriteNumber;
+//   }
+// }
+
+class Person {
+  constructor(firstName, lastName, favoriteColor, favoriteNumber){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.favoriteColor = favoriteColor;
+    this.favoriteNumber = favoriteNumber;
+    this.family = [];
+  }
+  fullName(){
+    return `${this.firstName} ${this.lastName}`
+  }
+  multiplyFavoriteNumber(num){
+    return num * this.favoriteNumber;
+  }
+  addToFamily(obj){
+    if(obj.constructor === Person && this.family.indexOf(obj) === -1){
+      this.family.push(obj)
+    }
+    return this.family
+  }
+}
+
+
+
+// Write a method called multiplyFavoriteNumber that takes in a number and returns the product of the number and the Person's favorite number
+
+// function Person(firstName, lastName, favoriteColor, favoriteNumber){
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.favoriteColor = favoriteColor;
+//   this.favoriteNumber = favoriteNumber;
+//   this.multiplyFavoriteNumber = function(num){
+//     return num * this.favoriteNumber;
+//   }
+// }
+
+class Person {
+  constructor(firstName, lastName, favoriteColor, favoriteNumber){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.favoriteColor = favoriteColor;
+    this.favoriteNumber = favoriteNumber;
+    this.family = [];
+  }
+  fullName(){
+    return `${this.firstName} ${this.lastName}`
+  }
+  multiplyFavoriteNumber(num){
+    return num * this.favoriteNumber;
+  }
+  addToFamily(obj){
+    if(obj.constructor === Person && this.family.indexOf(obj) === -1){
+      this.family.push(obj)
+    }
+    return this.family
+  }
+}
+
+// maps
+
+class MessageBoard {
+    constructor(){
+        this.messages = new Map
+        this.id = 1;
+    }
+    addMessage(value){
+        this.messages.set(this.id, value);
+        this.id++
+        return this;
+    }
+    findMessageById(id){
+      return this.messages.get(id)
+    }
+    findMessageByValue(val){
+      for (let msg of this.messages.values()) {
+        if(msg === val) return msg;
+      }
+    }
+    removeMessage(id){
+        this.messages.delete(id);
+        return this;
+    }
+    numberOfMessages(){
+        return this.messages.size;
+    }
+    messagesToArray(){
+        return Array.from(this.messages.values())
+    }
+}
+
+// sets
+function uniqueValues(arr){
+  return new Set(arr).size
+}
+
+function hasDuplicates(arr){
+  return new Set(arr).size !== arr.length
+}
+
+function countPairs(arr, num){
+    var cache = new Set(arr);
+    var count = 0;
+    for(let val of arr){
+        cache.delete(val)
+        if(cache.has(num - val)){
+            count++
+        }
+    }
+    return count;
+}
